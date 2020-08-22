c00a2c3c6fa47b12d91406bbd2c14d91b0936469
Arjun <cosmicpepe2001@gmail.com>
2020-08-22 21:24:50 +0530

    init commit

---
+++ src/emails/account.js
@@ -0,0 +1,24 @@
+const sgMail = require('@sendgrid/mail');
+const sendGridAPIKey = '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';
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const sendWelcomeEmail = (email,name) =>{
+    sgMail.send({
+        to: email,
+        from: 'cosmicpepe2001@gmail.com',
+        subject: 'Thanks for joining in',
+        text: `Welcome to the app, ${name}.`
+    });
+};
+
+const sendDeletionEmail = (email,name) =>{
+    sgMail.send({
+        to: email,
+        from: 'cosmicpepe2001@gmail.com',
+        subject: "We're sorry to see you go ;_;",
+        text: `Please let us know your reasons for cancellation and how we can improve by filling this form , ${name}.`
+    });
+};
+
+module.exports = {
+    sendWelcomeEmail,sendDeletionEmail
+};
---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+require('./db/mongoose');
+const jwt = require('jsonwebtoken');
+const app = express();
+const port = process.env.PORT;
+
+const Task = require('./models/task');
+const User = require('./models/user');
+
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
+
+app.use(userRouter);
+app.use(taskRouter);
+
+
+app.listen(port, ()=>{
+    console.log('Server is up on port ' + port)
+});
+
+
+
+
+
+
+
